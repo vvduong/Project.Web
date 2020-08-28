26387017ef8b22538c9dca35c26027715d68944b
vvduong <vvduong@lacviet.com.vn>
2020-08-28 22:23:15 +0700

    init

---
+++ Project.Web/webpack.config.js
@@ -0,0 +1,10 @@
+﻿var path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, 'App'),
+    entry: './main.js',
+    output: {
+        path: path.join(__dirname, 'Built'),
+        filename: '[name].bundle.js'
+    }
+};
